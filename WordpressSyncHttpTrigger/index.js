924ae3bea3ad5b90156d11f940e200ca6518e341
Carter Medlin <carterm@users.noreply.github.com>
2021-07-20 13:16:55 -0700

    content type

--- WordpressSyncHttpTrigger/index.js
+++ WordpressSyncHttpTrigger/index.js
@@ -5,6 +5,9 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: JSON.stringify(log,null,2)
+        body: JSON.stringify(log,null,2),
+        headers: {
+            "Content-Type": "application/json"
+        }
     };
 }
