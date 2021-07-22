9a11db89827385fe029511bfebfa8185d4f63119
Carter Medlin <carterm@users.noreply.github.com>
2021-07-22 12:47:39 -0700

    active sync

--- WordpressSyncHttpTrigger/index.js
+++ WordpressSyncHttpTrigger/index.js
@@ -1,18 +1,30 @@
 // @ts-check
+const { SyncEndpoint } = require('../wordpress-to-github');
 const { slackBotReportError,slackBotChatPost } = require('../common/slackBot');
 const log = [];
 //const debugChannel = 'C01DBP67MSQ'; // #testingbot
 const debugChannel = 'C01H6RB99E2'; //Carter debug
+const gitHubCommitter = {
+    name: process.env["GITHUB_NAME"],
+    email: process.env["GITHUB_EMAIL"]
+};
+const gitHubCredentials = { 
+    token: process.env["GITHUB_TOKEN"] 
+};
 
 /**
  * 
  * @param {{executionContext:{functionName:string},res:{body:any,headers:{"Content-Type":string}}}} context 
- * @param {{method:string,headers:{"user-agent":string},query:{},params:{},body:any}} req 
+ * @param {{method:string,headers:{"user-agent":string},query:{},params:{},body:import('../wordpress-to-github/common').GitHubTarget}} req 
  */
 module.exports = async function (context, req) {
     const appName = context.executionContext.functionName;
     try {
         await slackBotChatPost(debugChannel,`\n\n*Request Logged*\n\`\`\`${JSON.stringify(req,null,2)}\`\`\``);
+        if(req.method==='POST') {
+            await SyncEndpoint(req.body, gitHubCredentials, gitHubCommitter);
+        }
+        
         log.unshift(req);
 
         context.res = {
