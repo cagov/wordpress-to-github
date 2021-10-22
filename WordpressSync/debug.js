8849e47141f18ddc294e4d6499a3fde483c25a2b
Carter Medlin <carterm@users.noreply.github.com>
2021-10-22 13:23:12 -0700

    Readme updates

--- WordpressSync/debug.js
+++ WordpressSync/debug.js
@@ -2,7 +2,7 @@
 const { Values } = require("../local.settings.json");
 Object.keys(Values).forEach(x => (process.env[x] = Values[x])); //Load local settings file for testing
 
-process.env.debug = true;
+process.env.debug = true; //set to false or remove to run like the real instance
 
 //run the indexpage async
 const indexCode = require("./index");
