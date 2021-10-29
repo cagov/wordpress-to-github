7787b039ef720c1e038d23448f43c427cddf6f21
Carter Medlin <carterm@users.noreply.github.com>
2021-10-29 12:33:41 -0700

    trying combo endpoints

---
+++ WordpressSyncOneService/index.js
@@ -0,0 +1,38 @@
+// @ts-check
+const {
+  slackBotReportError,
+  slackBotChatPost,
+  slackBotReplyPost,
+  slackBotReactionAdd
+} = require("../common/slackBot");
+const debugChannel = "C01H6RB99E2"; //#carter-dev
+
+module.exports = async function (context, req) {
+  const appName = context.executionContext?.functionName;
+  let slackPostTS = "";
+  try {
+    slackPostTS = (
+      await (
+        await slackBotChatPost(
+          debugChannel,
+          `\n\n*Full Details*\n\`\`\`${JSON.stringify(req, null, 2)}\`\`\``
+        )
+      ).json()
+    ).ts;
+
+    context.res = {
+      body: `${JSON.stringify(req, null, 2)}`
+    };
+  } catch (e) {
+    await slackBotReplyPost(
+      debugChannel,
+      slackPostTS,
+      `\n\n*Full Details*\n\`\`\`${JSON.stringify(req, null, 2)}\`\`\``
+    );
+
+    context.res = {
+      status: 500,
+      body: `Error - ${e.message}`
+    };
+  }
+};
