f003df21af0c4145f11f5b77ac8f7bb1b4dd25d6
Jon Jensen <github@id.xjensen.com>
2021-11-01 16:51:43 -0700

    Repeat debugger to test caching

--- WordpressSync/debug.js
+++ WordpressSync/debug.js
@@ -3,13 +3,17 @@ const { Values } = require("../local.settings.json");
 Object.keys(Values).forEach(x => (process.env[x] = Values[x])); //Load local settings file for testing
 
 process.env.debug = true; //set to false or remove to run like the real instance
+const repeatCount = parseInt(process.argv.slice(2));
 
 //run the indexpage async
 const indexCode = require("./index");
 (async () => {
-  return await indexCode(
-    { executionContext: { functionName: "debug" } },
-    null,
-    []
-  );
+  for (let step = 0; step < repeatCount; step++) {
+    console.log(`****** Iteration ${step+1} ******`)
+    await indexCode(
+      { executionContext: { functionName: "debug" } },
+      null,
+      []
+    );
+  } 
 })();
